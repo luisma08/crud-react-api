8d2b6df4d524e50e5af8e3d0a5a922c62f357157
Jorge Mayta <luismayta08@gmail.com>
2023-01-16 02:34:50 -0500

    creando proyecto crud con react y api sql

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import {  Routes, Route, BrowserRouter } from "react-router-dom";
+import ShowProducts from './components/ShowProducts';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<ShowProducts />}>
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
